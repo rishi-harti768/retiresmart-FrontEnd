b976b4bc958e964a7986fcd7292137db79d057c0
ayushsingh199 <ayushsingh0319@gmail.com>
2024-11-25 02:32:16 +0530

    adding Intro section

---
+++ src/components/home/section-intro/SetionIntro.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const SetionIntro = () => {
+    return (
+        <>
+            <section className="intro">
+                <div className="wave-text">
+                    ~ Welcome to RetirSmart
+                </div>
+                <h1 className="main-title">Plan Smart, Live Smart</h1>
+                <p className="subtitle">Securing Your Future, One Step at a Time </p>
+                <p className="subtitle">Empowering You to Achieve Financial Freedom</p>
+
+                <button className="cta-button">Get Started</button>
+            </section>
+        </>
+    )
+}
+
+export default SetionIntro
