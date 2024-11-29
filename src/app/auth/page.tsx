3d6a36a4f7b4f5d0d5680cfb63af4189b2bc272f
ayushsingh199 <ayushsingh0319@gmail.com>
2024-11-29 23:17:20 +0530

    merge

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,14 +1,17 @@
 "use client";
-import SignIn from "@/components/Auth/SignIn/SignIn";
+
+
+import Authentication from "@/components/Auth/Authentication/Authentication";
 import "./page.css";
 
 import Default from "@/components/default/Default";
+
 
 export default function Page() {
   return (
     <>
       <Default current="Login">
-      <SignIn />
+      <Authentication />
       </Default>
     </>
   );
