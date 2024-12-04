1b9a1b79e26879f3b38108db95ea5fd51ddae471
Suhassk205 <suhassk2005@gmail.com>
2024-12-05 02:49:55 +0530

    Added code for footer sectiion

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,9 +3,43 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <>
-      <footer>Footer</footer>
-    </>
+    <footer>
+      {/* Section 1: Brand Name and Tagline */}
+      <div className="footer-brand">
+        <h2>RetirSmart</h2>
+        <p>"Your Gateway to Financial Freedom"</p>
+        <div className="social-links">
+          <a href="#"><img src="/icons/logos/twitterlogo.png" alt="Twitter" className="twitter" /> Twitter</a>
+          <a href="#"><img src="/icons/logos/youtubelogo.png" alt="YouTube" className="youtube" /> YouTube</a>
+          <a href="#"><img src="/icons/logos/instagramlogo.png" alt="Instagram" className="instagram" /> Instagram</a>
+          <a href="#"><img src="/icons/logos/linkedinlogo.png" alt="LinkedIn" className="linkedin" /> LinkedIn</a>
+        </div>
+        </div>
+
+      {/* Section 2: Quick Links */}
+      <div className="footer-links">
+        <h3>Quick Links</h3>
+        <p>
+          <a href="#">Home</a> | <a href="#">Contact Us</a> | <a href="#">About Us</a> |{" "}
+          <a href="#">FAQ</a>
+        </p>
+      </div>
+
+      {/* Section 3: Address */}
+      <div className="footer-address">
+        <h3>Our Address</h3>
+        <p>RetirSmart HQ</p>
+        <p>Velankani Road,</p>
+        <p>Electronic City, BLR, 560100, INDIA</p>
+        <p>Phone: xxx xxx xxx</p>
+        <p>Email: retirsmart05@gmail.com</p>
+      </div>
+
+      {/* Footer Note */}
+      <div className="footer-note">
+        <p>© 2024 RetirSmart. All rights reserved.</p>
+      </div>
+    </footer>
   );
 };
 
