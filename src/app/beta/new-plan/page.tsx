38dce8c9c36446170ce85e9c56d8bb8b71ba6077
ayushsingh199 <ayushsingh0319@gmail.com>
2024-12-04 19:19:22 +0530

    input page done

--- src/app/beta/new-plan/page.tsx
+++ src/app/beta/new-plan/page.tsx
@@ -1,9 +1,17 @@
-import Default from "@/components/default/Default";
+'use client'
 
-export default function Page() {
-  return (
-    <Default current="New Plan">
-      <h1>New plan</h1>
-    </Default>
-  );
+import React, { useState } from 'react'
+import './page.css'
+import Default from '@/components/default/Default'
+import InputForm from './InputForm'
+
+const Page = () => {
+  return(<>
+  <Default current='beta-input'>
+    <InputForm></InputForm>
+  </Default>
+  </>)
 }
+
+export default Page
+
